2cb087ff16bbb8d6e8daa1209232da2a6295ef72
admin_217 <uchqunovabdulloh44@gmail.com>
2024-04-29 22:37:09 -0700

    finish babel

--- my_ds_babel.py
+++ my_ds_babel.py
@@ -1,45 +1,55 @@
 import sqlite3
-import csv
-import pandas as pd 
+from mylib import add_table, creat_column, open_csv, column, make_question
+import os
 
-# def sql_to_csv(database, table_name):
-#   connection = sqlite3.connect(database)
-#   curs = connection.cursor()
-#   select = f"SELECT * FROM {table_name};"
-#   request = pd.read_sql_query(select, connection)
-#   request.to_csv("list_fault_lines.csv", index = False)
-#   with open("list_fault_lines.csv", "r") as file:
-#     all_results = file.read()
-#   return all_results[:-1]
-# print(sql_to_csv("all_fault_line.db", "fault_lines"))
 
-def csv_to_sql(csv_content, database, table_name):
-    for_description = []
-    csv_read = pd.read_csv(csv_content)
-    for_iloc = csv_read.iloc[:].values
-    connected = sqlite3.connect(database)
-    curs = connected.cursor()
-    curs.execute(f"CREATE TABLE {table_name} ('Volcano Name', 'Country', 'Type', 'Latitude (dd)', 'Longitude (dd)', 'Elevation (m)')")
-    curs.executemany("INSERT INTO volcanos VALUES (?, ?, ?, ?, ?, ?)", for_iloc)
-    connected.commit()
-    curs.execute(f"SELECT * FROM {table_name}")
-    for i in curs.description:
-        for_description.append(i)
-    return for_description
-    # connected = 
-# print(csv_to_sql("list_volcano.csv", "list_volcano.db", "volcanos"))
-    # return 
-# database = "all_fault_line.db"
-# connection = sqlite3.connect(database)
-# curs = connection.cursor()
-# select = "SELECT * FROM fault_lines;"
-# request = pd.read_sql_query(select, connection)
+def sql_to_csv(database, table_name):
+    database = sqlite3.connect(database)
 
-# print(select)
+    table = database.execute(f"SELECT * FROM {table_name};")
+    table = table.fetchall()
+    column = database.execute(f'SELECT name FROM pragma_table_info("{table_name}");')
+    column = column.fetchall()
 
-# with open("list_fault_lines.csv", "r") as file:
-#     all_results = file.read()
-#     print(all_results)
+    database.close()
 
-# a = pd.read_csv('list_fault_lines.csv')
-# print(a.isna())
+    csv = creat_column(column)
+    csv = add_table(table[0:-2], csv)
+
+    for n in range(len(table[-1])):
+        if n != len(table[-1]) - 1:
+            csv += f"{table[-1][n]},"
+        else:
+            csv += f"{table[-1][n]}\n"
+    return csv
+
+# csv_content = sql_to_csv("all_fault_line.db", "fault_lines")
+# # print(csv_content[0:1000])
+
+
+def csv_to_sql(csv_content, database, table_name): 
+    csv_content = open_csv(csv_content)
+
+    conection = sqlite3.connect(database)
+
+    cursor = conection.cursor()
+    cursor.execute(f"create table if not exists {table_name} ({column(csv_content[0])})")
+    # data = [(1, "Ridesharing", 'None', 'None', 'None', 'None'), (2, "Water Purifying", 'None', 'None', 'None', 'None'), (3, "Forensics", 'None', 'None', 'None', 'None'), (4, "Botany", 'None', 'None', 'None', 'None')]
+    cursor.executemany(f"INSERT INTO {table_name} VALUES({make_question(csv_content[1])})", csv_content[1:])
+
+    conection.commit()
+    conection.close()
+
+
+# csv_content = open('list_volcano.csv')
+# csv_to_sql(csv_content, 'list_volcanos.db','volcanos')
+
+
+# conection = sqlite3.connect('list_volcanos.db')
+# cursor = conection.cursor()
+# for row in cursor.execute(f"select * from volcanos"):
+#     print('...')
+#     print(row)
+# conection.close()
+
+# os.remove('list_volcanos.db')
